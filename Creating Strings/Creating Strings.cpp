c5c72f595f864199086d0b6087f1f68ac774ace5
Svar7769 <svarpatel7749@gmail.com>
2021-05-30 00:05:50 +0530

    Creating String

---
+++ Creating Strings/Creating Strings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    vector<string> permutation;
+
+    sort(str.begin(), str.end());
+
+    do {
+
+        permutation.push_back(str); 
+    }while ( next_permutation(str.begin(), str.end()));
+
+    cout << permutation.size() << endl;
+
+    for ( int i = 0; i < permutation.size(); i++) {
+        cout << permutation[i] << endl;
+    }
+}
