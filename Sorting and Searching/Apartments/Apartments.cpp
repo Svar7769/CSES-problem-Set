43f66d58e81188fe98a951a58b850dd16ad6b385
Svar7769 <svarpatel7749@gmail.com>
2021-06-04 00:33:32 +0530

    Apartments

--- Sorting and Searching/Apartments/Apartments.cpp
+++ Sorting and Searching/Apartments/Apartments.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 #include <algorithm>
 
-#define  ll long long
+#define ll long long
 
 using namespace std;
 
-int main() {
-    int n,m,ways=0;
-    ll k;
+int main()
+{
+    int n, m;
+    ll k, ways=0;
 
-    cin >> n >> m >> k ;
+    cin >> n >> m >> k;
 
-    ll a[n], b[m], no;
+    ll a[n], b[m];
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> a[i];
     }
-    for (int i = 0; i < m; i++)    {
+    for (int i = 0; i < m; i++)
+    {
         cin >> b[i];
     }
 
-    sort(a, a+n);
-    sort(b,b+m);
+    sort(a, a + n);
+    sort(b, b + m);
 
-    int lastpos = 0;
+    int i=0,j=0;
 
-    for (int i = 0; i < m; i++) {
-        for(int j = lastpos; j<n; j++) {
-            if (a[j] >= b[i]-k && a[j] <= b[i]+k)
-            {
-                ways++;
-                lastpos = j+1;
-                break;
-            }
-            if (b[i] <= a[i]-k) {
-                break;
+    while (i<n && j<m) {
+        if (abs(a[i]-b[j]) <= k) {
+            ++j; ++i;
+            ++ways;
+        }
+        else{
+            if(a[i]-b[j] > k) {
+                ++j;
+            }else {
+                i++;
             }
         }
     }
